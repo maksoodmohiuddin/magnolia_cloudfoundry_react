ca267dcf896918930019ed2b45270d058ae5f3ad
Maksood Mohiuddin <maksood@gmail.com>
2020-08-20 17:11:33 -0400

    Added build folder for non-build PCF deployment support

---
+++ build/precache-manifest.4ec885540cd84903f97d1fead6dce903.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2d920c5061ef87b4016e48274538f64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c778506f8361cee4cc1f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e5aecb373f6697191faa",
+    "url": "/static/js/2.979e1334.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.979e1334.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c778506f8361cee4cc1f",
+    "url": "/static/js/main.15fa582e.chunk.js"
+  },
+  {
+    "revision": "e79b09f7301f298fcee2",
+    "url": "/static/js/runtime-main.3c9e235e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
